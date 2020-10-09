dda0fce7844256c7acb082da5b25e1a79672171f
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2020-10-09 10:42:58 +0200

    a

---
+++ PROP/Kakuro/Dominio/Usuario.java
@@ -0,0 +1,19 @@
+package Dominio;
+
+public class Usuario {
+    public Usuario()
+    {
+
+    }
+
+    public Boolean register(String name, String password)
+    {
+
+    }
+
+    //devuelve FALSE si el usuario no existe o la contraseña es incorrecta
+    public Boolean login(String name, String password)
+    {
+        
+    }
+}
